ccbf4cc4c55e0ccd3b15a806d6a8c09d39179378
unknown <bbashemera@gmail.com>
2019-02-24 11:04:32 -0800

    feat:Challenge 2 dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+def displayKeyValuePairs():
+    dictionary=dict()
+    for dictionaryvalues in range(1,16):
+        dictionary[dictionaryvalues]=dictionaryvalues**2
+    print(dictionary)
+
+if __name__=="__main__":
+    displayKeyValuePairs()
